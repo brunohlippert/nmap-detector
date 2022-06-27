70449e78585b28c1b7ae440663d7987c4617834b
Victor Putrich <scherer.victor98@gmail.com>
2022-06-27 19:57:01 -0300

    s

--- runDetector.sh
+++ runDetector.sh
@@ -1,9 +1,8 @@
 rm detector
 gcc -Wall -c message/message.c -w
 gcc -Wall -c detector.c -w
-gcc -Wall -c attack_statemachines.c -w
 
-gcc -o detector message.o detector.o attack_statemachines.o -lpthread
+gcc -o detector message.o detector.o -lpthread
 rm *.o
 
-sudo ./detector enp4s0
+sudo ./detector wlp0s20f3
